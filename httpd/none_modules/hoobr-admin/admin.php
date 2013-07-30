d987b2eb738bc1d1ffe77ba6f328032406948ab4
Ric Allinson <capecodehq@yahoo.co.uk>
2013-07-30 13:49:41 -0700

    Remove hoobr-app module

--- httpd/none_modules/hoobr-admin/admin.php
+++ httpd/none_modules/hoobr-admin/admin.php
@@ -1,13 +1,41 @@
 <?php
-require("../hoobr-app/index.php");
+require("../../node_modules/php-require/index.php");
 
 /*
-    @route GET|POST /admin
+    Do some error logging.
 */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+/*
+    Grab the $request, $response objects.
+*/
+
+$res = $require("php-http");
+$req = $res->request; // done for convenience.
+
+/*
+    Trigger middleware.
+*/
+
+$require("../hoobr-users/middleware/auth");
+
+/*
+    Set the renderer to be used by default.
+*/
+
+$res->renderer[".php.html"] = $require("php-render-php");
+
+/*
+    Grab the composite modules for building the page.
+*/
+
 
 $composite = $require("php-composite");
 
 /*
+    @route GET|POST /admin
     If the user is not logged in show only the login form.
 */
 
--- httpd/none_modules/hoobr-public/public.php
+++ httpd/none_modules/hoobr-public/public.php
@@ -1,13 +1,40 @@
 <?php
-require("../hoobr-app/index.php");
+require("../../node_modules/php-require/index.php");
 
 /*
-    @route GET /
+    Do some error logging.
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+/*
+    Grab the $request, $response objects.
+*/
+
+$res = $require("php-http");
+$req = $res->request; // done for convenience.
+
+/*
+    Trigger middleware.
+*/
+
+$require("../hoobr-users/middleware/auth");
+
+/*
+    Set the renderer to be used by default.
+*/
+
+$res->renderer[".php.html"] = $require("php-render-php");
+
+/*
+    Grab the composite modules for building the page.
 */
 
 $composite = $require("php-composite");
 
 /*
+    @route GET /
     Renders the main page.
 */
 
